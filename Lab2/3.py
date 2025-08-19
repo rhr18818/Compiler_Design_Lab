0963e74a9e9caa23f8b0e7b9b5495f7ec2301822
rhr18818 <rhr14101@gmail.com>
2025-08-20 02:43:48 +0600

    Complier lab2 task ---- 3

---
+++ Lab2/3.py
@@ -0,0 +1,13 @@
+s = input("Enter a string with only 'a' and 'b': ")
+
+i = 0
+while i < len(s) and s[i] == 'a':
+    i += 1
+
+while i < len(s) and s[i] == 'b':
+    i += 1
+
+if i == len(s):
+    print("Valid: Matches a^m b^n")
+else:
+    print("Invalid: a’s must come before b’s only")
